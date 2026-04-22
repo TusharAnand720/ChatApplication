8e52dff0b0269ba303bd76f558d9824d667ad4ff
TusharAnand720 <anand.tushar720@gmail.com>
2026-04-22 13:46:40 +0530

    Import lib added in Applications && jwt config added in application.property && auth lib updated

--- src/main/java/chatapp/Application.java
+++ src/main/java/chatapp/Application.java
@@ -1,13 +1,16 @@
 package chatapp;
 
+import authorization.lib.config.AuthAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(AuthAutoConfiguration.class)
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
